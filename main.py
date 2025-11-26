8ab405d7d7114f02d453cbb56501ee10fbc35e7b
wikkutek <zszczerbiak.neuca@gmail.com>
2025-11-26 17:46:57 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+
+a = 10
+
+
+b = 5
+
+c = 1
+
+
+# git init — utworzenie nowego repozytorium
+# git status — sprawdzenie obecnego wyglądu repozytorium
+# git add nazwa_pliku - dodaj plik do repozytorium
+# git add . - dodaj wszystkie pliki i foldery do repo
+# cls - wyczyszczenie widoku
+# git commit -m "wiadomość" -
+
+
+
+
+
